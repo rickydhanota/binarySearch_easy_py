a86a1212cafa7bfa8c6c5584e5062fc1cee83d61
Ricky <rickdhanota@gmail.com>
2020-10-29 02:14:38 -0700

    initial commit

---
+++ binary_search_easy.py
@@ -0,0 +1,8 @@
+# Binary Search
+# Write a function that takes in a sorted array of integers as well as a target integer. The function should use the bonary search algorithm to determine if the target integer is container in the arry and should return its index if it is, otherwise -1
+#array = [0,1, 2, 21, 33, 45, 45, 61, 71. 72, 73]
+#target = 33
+#output = 3
+
+def binarySearch(array, target):
+    pass
