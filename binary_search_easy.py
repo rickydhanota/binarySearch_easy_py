fc78561905ebc09b84e35c1f273cb106d3346235
Ricky <rickdhanota@gmail.com>
2020-10-29 02:18:53 -0700

    bugs i need to fix

--- binary_search_easy.py
+++ binary_search_easy.py
@@ -1,8 +1,22 @@
 # Binary Search
 # Write a function that takes in a sorted array of integers as well as a target integer. The function should use the bonary search algorithm to determine if the target integer is container in the arry and should return its index if it is, otherwise -1
-#array = [0,1, 2, 21, 33, 45, 45, 61, 71. 72, 73]
+#array = [0,1, 2, 21, 33, 45, 45, 61, 71, 72, 73]
 #target = 33
 #output = 3
 
 def binarySearch(array, target):
-    pass
+    return binarySearchHelper(array, target, 0, len(array)- 1)
+
+def binarySearchHelper(array, target, left, right):
+    while left <= right:
+        middle = (left + right) // 2
+        potentialMatch = array[middle]
+        if target == potentialMatch:
+            return middle
+        elif target < potentialMatch:
+            right = middle - 1
+        else:
+            left = middle - 1
+    return -1
+
+print(binarySearch([0,1, 2, 21, 33, 45, 45, 61, 71, 72, 73], 33))
